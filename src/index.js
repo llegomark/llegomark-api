72480e96bef5e875bd91c81fb6c2cad76ed0d6db
Mark Anthony Llego <lr.mallego@mmdc.mcl.edu.ph>
2023-03-07 15:51:09 +0800

    refactor

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import moment from 'moment';
 
 const UPSTREAM_URL = 'https://api.openai.com/v1/chat/completions';
-const ORG_ID_REGEX = /\borg-[a-zA-Z0-9]{24}\b/g; // used to obfuscate any org IDs in the response text
-const MAX_REQUESTS = 100; // maximum number of requests per IP address per hour
+const ORG_ID_REGEX = /\borg-[a-zA-Z0-9]{24}\b/g;
+const MAX_REQUESTS = 20;
 
 const CORS_HEADERS = {
-  'Access-Control-Allow-Origin': '', // add your domain here
-  'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS',
+  'Access-Control-Allow-Origin': 'https://chat.llego.dev',
+  'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, BREW',
   'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-  'Vary': 'Origin', // add vary header to inform clients that CORS is enabled.
 };
 
 const STREAM_HEADERS = {
@@ -16,110 +15,81 @@ const STREAM_HEADERS = {
   'Connection': 'keep-alive',
 };
 
-const ALLOWED_DOMAINS = ['llego.dev'];
-
-// Define an async function that hashes a string with SHA-256
 const sha256 = async (message) => {
   const data = new TextEncoder().encode(message);
   const hashBuffer = await crypto.subtle.digest('SHA-256', data);
-  return Array.from(hashBuffer)
-    .map((b) => b.toString(16).padStart(2, '0'))
-    .join('');
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  return hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
 };
 
-const randomChoice = (arr) => {
-  if (arr.length === 0) {
-    throw new Error('Cannot get a random choice from an empty array');
+const randomChoice = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+const obfuscateOpenAIResponse = (text) => text.replace(ORG_ID_REGEX, 'org-************************').replace(' Please add a payment method to your account to increase your rate limit. Visit https://platform.openai.com/account/billing to add a payment method.', '');
+
+const hashIp = (ip, utcNow, secret_key) => sha256(`${utcNow.format('ddd=DD.MM-HH+YYYY')}-${ip}:${secret_key}`);
+
+const handleRateLimit = async (request, env, utcNow, clientIpHash) => {
+  const rateLimitKey = `rate_limit_${clientIpHash}`;
+  const rateLimitExpiration = utcNow.startOf('hour').add(1, 'hour').unix();
+  const { rateLimitCount = 0 } = (await env.kv.get(rateLimitKey, { type: 'json' })) || {};
+
+  if (rateLimitCount > MAX_REQUESTS) {
+    return new Response('You have exceeded the maximum limit of 20 requests. Please try again in one hour.', { status: 429, headers: CORS_HEADERS });
   }
-  const randomIndex = Math.floor(Math.random() * arr.length);
-  return arr[randomIndex];
+
+  await env.kv.put(rateLimitKey, JSON.stringify({ rateLimitCount: rateLimitCount + 1 }), { expiration: rateLimitExpiration });
 };
 
-// Define an async function that hashes user IP address, UTC year, month, day, day of the week, hour and the secret key
-// To implement IP-based rate limiting, we have to store users' IP addresses in a certain way. However, we want to protect
-// users' privacy as much as possible. To achieve this, we use SHA-256 to calculate a digest value of the user's IP address
-// along with the UTC year, month, day, day of the week, hour, and the secret key. The resulting digest not only depends on
-// the user's IP address but is also unique to each hour, making the user's IP address hard to be determined. Moreover, the
-// one-way nature of the SHA-256 algorithm implies that even if the digest value is compromised, it is almost impossible to
-// reverse it to obtain the original IP address, ensuring the privacy and security of the user's identity.
-const hashIp = async (ip, utcNow, secret_key) => {
-  const message = `${utcNow.format('ddd=DD.MM-HH+YYYY')}-${ip}:${secret_key}`;
-  try {
-    const hash = await sha256(message);
-    return hash;
-  } catch (error) {
-    console.error(`Error hashing message: ${message}`, error);
-    throw new Error('Error hashing IP address');
+const handleApiKeySelection = (env) => {
+  return randomChoice(JSON.parse(env.API_KEYS));
+};
+
+const handleUpstreamRequest = async (requestBody, api_key) => {
+  const upstreamResponse = await fetch(UPSTREAM_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${api_key}`,
+      'User-Agent': 'curl/7.88.1',
+    },
+    body: JSON.stringify(requestBody),
+  });
+
+  if (!upstreamResponse.ok) {
+    const { status } = upstreamResponse;
+    const text = await upstreamResponse.text();
+    const textObfuscated = obfuscateOpenAIResponse(text);
+    return new Response(`Mark Anthony Llego API responded with:\n\n${textObfuscated}`, { status, headers: CORS_HEADERS });
   }
-};
 
-// Define a separate utility function that calls sha256 to hash User's IP and the current date and time.
-const getIpHash = async (request, env) => {
-  const utcNow = moment.utc();
-  const clientIp = request.headers.get('CF-Connecting-IP');
-  const clientIpHash = await hashIp(clientIp, utcNow, env.SECRET_KEY);
-  return clientIpHash;
+  return upstreamResponse;
 };
 
 const handleRequest = async (request, env) => {
+  let requestBody;
+
   try {
-    // Code for handling request
-    let requestBody = null;
-    try {
-      requestBody = await request.json();
-    } catch (error) {
-      return new Response('Malformed JSON', { status: 422, headers: CORS_HEADERS });
-    }
+    requestBody = await request.json();
+  } catch (error) {
+    return new Response('Malformed JSON', { status: 422, headers: CORS_HEADERS });
+  }
 
-    const { stream } = requestBody;
-    if (stream != null && stream !== true && stream !== false) {
-      return new Response('The `stream` parameter must be a boolean value', { status: 400, headers: CORS_HEADERS });
-    }
+  const { stream } = requestBody;
 
-    // Check if the request is coming from an allowed domain
-    const origin = request.headers.get('Origin');
-    if (origin == null || !ALLOWED_DOMAINS.some((allowed) => origin.endsWith(allowed))) {
-      return new Response('Origin Not Allowed', { status: 403, headers: CORS_HEADERS });
-    }
+  if (stream != null && stream !== true && stream !== false) {
+    return new Response('The `stream` parameter must be a boolean value.', { status: 400, headers: CORS_HEADERS });
+  }
 
-    // Enforce the rate limit based on hashed client IP address
+  try {
     const utcNow = moment.utc();
-    const clientIpHash = await getIpHash(request, env);
-    const rateLimitKey = `rate_limit_${clientIpHash}`;
-    const rateLimitData = (await env.kv.get(rateLimitKey, { type: 'json' })) || {};
-    const { rateLimitCount = 0, rateLimitExpiration: oldRateLimitExpiration } = rateLimitData;
-    const newRateLimitExpiration = utcNow.hour() === moment.unix(oldRateLimitExpiration).hour() ? moment.unix(oldRateLimitExpiration).add(1, 'hour').unix() : utcNow.startOf('hour').add(1, 'hour').unix();
-    const rateLimitExpiration = newRateLimitExpiration;
+    const clientIp = request.headers.get('CF-Connecting-IP');
+    const clientIpHash = await hashIp(clientIp, utcNow, env.SECRET_KEY);
 
-    if (rateLimitCount > MAX_REQUESTS) {
-      return new Response('Please try again in an hour as we have received an excessive amount of requests.', { status: 429, headers: CORS_HEADERS });
-    }
+    await handleRateLimit(request, env, utcNow, clientIpHash);
 
-    // Forward a POST request to the upstream URL and return the response
-    const api_key = randomChoice(JSON.parse(env.API_KEYS));
-    const upstreamResponse = await fetch(UPSTREAM_URL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${api_key}`,
-        'User-Agent': 'curl/7.64.1',
-      },
-      body: JSON.stringify(requestBody),
-    });
+    const api_key = handleApiKeySelection(env);
 
-    if (!upstreamResponse.ok) {
-      const { status } = upstreamResponse;
-      const text = await upstreamResponse.text();
-      const obfuscatedText = text.replace(ORG_ID_REGEX, 'org-************************');
-      return new Response(`OpenAI API responded with:\n\n${obfuscatedText}`, { status, header: CORS_HEADERS });
-    }
-
-    // Update the rate limit information
-    const rateLimitDataNew = {
-      rateLimitCount: rateLimitCount + 1,
-      rateLimitExpiration,
-    };
-    await env.kv.put(rateLimitKey, JSON.stringify(rateLimitDataNew), { expiration: rateLimitExpiration });
+    const upstreamResponse = await handleUpstreamRequest(requestBody, api_key);
 
     return new Response(upstreamResponse.body, {
       headers: {
@@ -129,51 +99,53 @@ const handleRequest = async (request, env) => {
       },
     });
   } catch (error) {
-    console.error(error);
-
-    let message = 'Internal Server Error';
-    let status = 500;
-
-    if (error instanceof Response) {
-      // If error is coming from upstream API, reflect it back to the client
-      message = await error.text();
-      status = error.status;
-
-      console.error(`Upstream API responded with:\n\n${message}`);
-    } else if (error instanceof TypeError) {
-      // If error is caused by a programming error like incorrect arguments, etc.
-      message = error.message;
-      status = 400;
-    }
-
-    return new Response(message, { status, headers: CORS_HEADERS });
+    return new Response(error.message, { status: 500, headers: CORS_HEADERS });
   }
+};
+
+const handlePreflightRequest = () => {
+  return new Response(null, {
+    headers: {
+      ...CORS_HEADERS,
+      'Access-Control-Max-Age': '1728000',
+    },
+  });
+};
+
+const handleInvalidContentType = () => {
+  return new Response("Unsupported media type. Use 'application/json' content type.", { status: 415, headers: CORS_HEADERS });
+};
+
+const handleInvalidMethod = () => {
+  return new Response('Method not allowed.', { status: 405, headers: CORS_HEADERS });
+};
+
+const handleInvalidResource = () => {
+  return new Response('You are not allowed to access this resource.', { status: 403, headers: CORS_HEADERS });
 };
 
 export default {
   async fetch(request, env) {
     const { pathname } = new URL(request.url);
+
     if (pathname !== '/v1/') {
-      return new Response('Not Found', { status: 404, headers: CORS_HEADERS });
+      return handleInvalidResource();
     }
+
     if (request.method === 'OPTIONS') {
-      return new Response(null, {
-        headers: {
-          ...CORS_HEADERS,
-          'Access-Control-Max-Age': '1728000',
-        },
-      });
+      return handlePreflightRequest();
     }
 
     if (request.method !== 'POST') {
-      return new Response('Method Not Allowed', { status: 405, headers: CORS_HEADERS });
+      return handleInvalidMethod();
     }
 
     const contentType = request.headers.get('Content-Type');
+
     if (!contentType || contentType !== 'application/json') {
-      return new Response("Unsupported media type. Use 'application/json' content type", { status: 415, headers: CORS_HEADERS });
+      return handleInvalidContentType();
     }
 
-    return await handleRequest(request, env);
+    return handleRequest(request, env);
   },
 };
